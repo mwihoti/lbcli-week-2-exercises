5948ba1f590d195be245fe7b59cc9d5cfc6afd9e
mwihoti <danielmwihoti@gmail.com>
2026-07-01 22:54:22 +0300

    fix

--- submission/09.sh
+++ submission/09.sh
@@ -1,31 +1,14 @@
-# Create a transaction whose fee can be later updated to a higher fee if it is stuck or doesn't get mined on time
-
-# Amount of 20,000,000 satoshis to this address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP
-# Use the UTXOs from the transaction below
-# raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 #!/bin/bash
 set -euo pipefail
+
 BASE_TX="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 TARGET="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-PAY_SATS=20000000
-FEE_SATS=10000
-RBF_SEQ=4294967293
 
 TXID=$(bitcoin-cli -regtest decoderawtransaction "$BASE_TX" | jq -r .txid)
 
-TOTAL_SATS=$(bitcoin-cli -regtest decoderawtransaction "$BASE_TX" | jq '[.vout[].value] | add | . * 1e8 | floor')
-CHANGE_SATS=$((TOTAL_SATS - PAY_SATS - FEE_SATS))
-
-if [ "$CHANGE_SATS" -lt 0 ]; then echo "ERROR: insufficient funds" >&2; exit 1; fi
-
-CHANGE_ADDR=$(bitcoin-cli -regtest getrawchangeaddress)
-toBTC(){ awk "BEGIN{printf \"%.8f\", $1/1e8}"; }
-PAY_BTC=$(toBTC $PAY_SATS)
-CHANGE_BTC=$(toBTC $CHANGE_SATS)
-
-INPUTS="[{\"txid\":\"$TXID\",\"vout\":0,\"sequence\":$RBF_SEQ},{\"txid\":\"$TXID\",\"vout\":1,\"sequence\":$RBF_SEQ}]"
-OUTPUTS="{\"$TARGET\":$PAY_BTC,\"$CHANGE_ADDR\":$CHANGE_BTC}"
+INPUTS="[{\"txid\":\"$TXID\",\"vout\":0,\"sequence\":4294967293},{\"txid\":\"$TXID\",\"vout\":1,\"sequence\":4294967293}]"
+OUTPUTS="{\"$TARGET\":0.2}"
 
 RAW=$(bitcoin-cli -regtest createrawtransaction "$INPUTS" "$OUTPUTS")
-SIGNED=$(bitcoin-cli -regtest signrawtransactionwithwallet "$RAW" | jq -r .hex)
-echo "$SIGNED"
+
+echo "$RAW"
