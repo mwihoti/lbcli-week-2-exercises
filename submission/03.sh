8be3430ae289da883937040b80f6200b85f91ce4
mwihoti <danielmwihoti@gmail.com>
2026-07-01 16:07:24 +0300

    fix sol3

--- submission/03.sh
+++ submission/03.sh
@@ -2,5 +2,5 @@
 # Add funds to the address.
 # Return only the Address
 ADDR=$(bitcoin-cli -regtest getnewaddress "" bech32)
-bitcoin-cli -regtest generatetoaddress 1 "$ADDR" > /dev/null
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR" > /dev/null
 echo "$ADDR"
